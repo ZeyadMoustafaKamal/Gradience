9553eae1ac17dccd9c80c3218e53a699c9560956
Zeyad Moustafa Kamal <zeyad20062018@gmail.com>
2025-01-31 18:34:37 +0200

    add installation files

---
+++ install_deps.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# install `gradience` dependencies in arch linux or arch based distros.
+sudo pacman -S gtk4 libadwaita libsoup3 libportal-gtk4 blueprint-compiler gobject-introspection sassc
+
+
