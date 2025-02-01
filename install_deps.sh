1114fd9d77293d4ff63cacd2117726081d34fe25
Zeyad Moustafa Kamal <zeyad20062018@gmail.com>
2025-02-01 18:26:56 +0200

    remove `install_deps.sh`

--- install_deps.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# install `gradience` dependencies in arch linux or arch based distros.
-sudo pacman -S gtk4 libadwaita libsoup3 libportal-gtk4 blueprint-compiler gobject-introspection sassc
-
-
